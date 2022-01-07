3c8f25750e8a778399457f5b6b327f252a2fab2b
brinkbrink <brinkbrink@gmail.com>
2022-01-07 07:54:08 -0800

    scaffolding

---
+++ conversion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Temperature Converter: IT 262 Group 1</title>
+<link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+<?php
+
+
+
+
+
+
+?>
+</body>
+
+</html>
