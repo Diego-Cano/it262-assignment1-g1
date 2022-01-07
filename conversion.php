520882f5e3a0f77f27050df9905f70fdcf8d8c64
brinkbrink <brinkbrink@gmail.com>
2022-01-07 09:10:40 -0800

    created basic page with form and added tentative if logic

--- conversion.php
+++ conversion.php
@@ -7,7 +7,122 @@
 </head>
 
 <body>
+<form action ="
+<?php echo htmlspecialchars($_SERVER['PHP_SELF'])  ;?>
+" method="post">
+<fieldset>
+    <legend>Group 1's Temperature Converter</legend>
+    <label for="degree">How many degrees?</label>
+
+ <input type = "text" name = "degree" size=4> 
+ 
+ <label for="ogScale">Original scale?</label>
+ <select name="ogScale"><option value="fahr">Fahrenheit</option><option value="cel">Celcius</option> <option value="kel">Kelvin</option> <option value="ran">Rankine</option> </select> 
+ <label for="newScale">Desired scale?</label>
+ <select name="newScale"><option value="fahr">Fahrenheit</option><option value="cel">Celcius</option> <option value="kel">Kelvin</option> <option value="ran">Rankine</option> </select> 
+ 
+
+ <input type = "submit" value = "Convert"/> 
+ </form> 
 <?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if(isset($_POST['degree'])) { // making sure user has filled out degree value
+    $degree = $_POST['degree'];
+    $ogScale = $_POST['ogScale'];
+    $newScale = $_POST['newScale'];
+
+    $f2c = ($degree - 32) * (5/9); // conversion formula
+    $round_f2c = number_format($f2c, 2); // rounding to two decimals
+
+    $f2k =  ((5/9) * $degree) + 459.67;
+    $round_f2k = number_format($f2k, 2);
+
+    $f2r = $degree + 459.67;
+    $round_f2r = number_format($f2r, 2);
+
+    $c2f = ($degree * (9/5)) + 32 ;
+    $round_c2f = number_format($c2f, 2);
+
+    $c2k = $degree + 273.15;
+    $round_c2k = number_format($c2k, 2);
+
+    $c2r = ($degree * (9/5)) + 491.67;
+    $round_c2r = number_format($c2r, 2);
+
+    $k2f = (($degree - 273.15) * (9/5)) + 32;
+    $round_k2f = number_format($k2f, 2);
+
+    $k2c = $degree - 273.15;
+    $round_k2c = number_format($k2c, 2);
+
+    $k2r = $degree / 1.8;
+    $round_k2r = number_format($k2r, 2);
+
+    $r2f = $degree - 459.67;
+    $round_r2f = number_format($c2k, 2);
+
+    $r2c = ($degree - 491.67) * (5/9);
+    $round_r2c = number_format($c2k, 2);
+
+    $r2k = $degree * 1.8;
+    $round_r2k = number_format($c2k, 2);
+
+    if(($degree != NULL) && (is_numeric($degree))){ // making sure degree value is filled and numeric
+
+        if (($ogScale == fahr) && ($newScale == cel)){ // Fahrenheit --> Celcius
+            echo $round_f2c;
+        }
+
+        if (($ogScale == fahr) && ($newScale == kel)){ // Fahrenheit --> Kelvin
+            echo $round_f2k;
+        }
+
+        if (($ogScale == fahr) && ($newScale == ran)){ // Fahrenheit --> Rankine
+            echo $round_f2r;
+        }
+
+        if (($ogScale == cel) && ($newScale == fahr)){ // Celcius --> Fahrenheit
+            echo $round_c2f;
+        }
+
+        if (($ogScale == cel) && ($newScale == kel)){ // Celcius --> Kelvin
+            echo $round_c2k;
+        }
+
+        if (($ogScale == cel) && ($newScale == ran)){ // Celcius --> Rankine
+            echo $round_c2r;
+        }
+
+        if (($ogScale == kel) && ($newScale == fahr)){ // Kelvin --> Fahrenheit
+            echo $round_k2f;
+        }
+
+        if (($ogScale == kel) && ($newScale == cel)){ // Kelvin --> Celcius
+            echo $round_k2c;
+        }    
+
+        if (($ogScale == kel) && ($newScale == ran)){ // Kelvin --> Rankine
+            echo $round_k2r;
+        }
+
+        if (($ogScale == ran) && ($newScale == fahr)){ // Rankine --> Fahrenheit
+            echo $round_k2f;
+        }
+
+        if (($ogScale == ran) && ($newScale == cel)){ // Rankine --> Fahrenheit
+            echo $round_k2f;
+        }
+
+        if (($ogScale == ran) && ($newScale == kel)){ // Rankine --> Fahrenheit
+            echo $round_k2f;
+        }
+
+    } 
+
+    }
+}
 
 
 
