fdda2d565f9690ba15413cfc265083a1538a73bd
Darrian DeVaughnn <devondevaughnn@gmail.com>
2022-01-09 10:04:56 -0800

    fixed === error

---
+++ conversion.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Temperature Converter: IT 262 Group 1</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+
+    <?php function cels_faren($temp)
+{
+    $faren = (((9 / 5) * $temp) + (32));
+    $faren_format = number_format($faren, 2);
+    return $faren_format;
+}
+function faren_kelv($temp)
+{
+    $kelvin = (($temp + 459.67) * (5 / 9));
+    $kelvin_format = number_format($kelvin, 2);
+    return $kelvin_format;
+}
+function faren_cels($temp)
+{
+    $cels = ((5 / 9) * ($temp - 32));
+    $cels_format = number_format($cels, 2);
+    return $cels_format;
+}
+function kelv_cels($temp)
+{
+    $kelv_cels = (($temp - 273.15));
+    $kelv_cels_format = number_format($kelv_cels, 2);
+    return $kelv_cels_format;
+}function cels_kelv($temp)
+{
+    $cels_kelv = (($temp + 273.15));
+    $cels_kelv_format = number_format($cels_kelv, 2);
+    return $cels_kelv_format;
+}function kelv_faren($temp)
+{
+    $kelv_faren = (($temp * (9 / 5)) - 459.67);
+    $kelv_faren_format = number_format($kelv_faren, 2);
+    return $kelv_faren_format;
+}
+
+if (isset($_POST['convert'])) {
+    $init_type = $_POST['init_type'];
+    $temp_type = $_POST['temp_type'];
+    $user_temp = $_POST['user_temp'];
+    if ($init_type == "farenheit" && $temp_type == "celsius") {
+        $conv = faren_cels($user_temp);
+        echo "$user_temp °F  is $conv °C.";
+
+    } elseif ($init_type == "farenheit" && $temp_type == "kelvin") {
+        $conv = faren_kelv($user_temp);
+        echo "$user_temp $init_type is $conv K.";
+
+    } elseif ($init_type == "celsius" && $temp_type == "farenheit") {
+        $conv = cels_faren($user_temp);
+        echo "$user_temp °C is $conv °F.";
+
+    } elseif ($init_type == "celsius" && $temp_type == "kelvin") {
+        $conv = cels_kelv($user_temp);
+        echo "$user_temp °C is $conv K.";
+
+    } elseif ($init_type == "kelvin" && $temp_type == "farenheit") {
+        $conv = kelv_faren($user_temp);
+        echo "$user_temp K is $conv °F.";
+
+    } elseif ($init_type == "kelvin" && $temp_type == "celsius") {
+        $conv = kelv_cels($user_temp);
+        echo "$user_temp K is $conv °C.";
+    } elseif ($init_type == $temp_type) {
+        echo "Please choose a different temperature type to convert.";
+    }
+}
+
+?>
+
+
+</body>
+
+</html>
--- includes/form.php
+++ includes/form.php
@@ -1,42 +1,30 @@
-<form action ="
-<?php echo htmlspecialchars($_SERVER['PHP_SELF'])  ;?>
-" method="post">
-<fieldset>
-    <legend>Group 1's Temperature Converter</legend>
-    <label for="degree">How many degrees?</label>
-    <input type = "text" name = "degree" size=4> 
-    <span class="error"><?php echo $degree_Err; ?></span>
- 
- <label for="ogScale">Original scale?</label>
- <select name="ogScale"><option value="" NULL
-<?php if(isset($_POST['ogScale']) && $_POST['ogScale'] == NULL) echo 'selected = "unselected" '
-;?>>
-<option value="fahr"<?php if(isset($_POST['ogScale']) && $_POST['ogScale'] == 'fahr') echo 'selected = "selected" '
-;?>>Fahrenheit</option>
-<option value="cel"<?php if(isset($_POST['ogScale']) && $_POST['ogScale'] == 'cel') echo 'selected = "selected" '
-;?>>Celcius</option> 
-<option value="kel"<?php if(isset($_POST['ogScale']) && $_POST['ogScale'] == 'kel') echo 'selected = "selected" '
-;?>>Kelvin</option> 
-<option value="ran"<?php if(isset($_POST['ogScale']) && $_POST['ogScale'] == 'ran') echo 'selected = "selected" '
-;?>>Rankine</option> 
-</select> 
-<span class="error"><?php echo $ogScale_Err; ?></span>
+<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+    <table>
+        <tr>
+            <td>Enter temperature.</td>
+            <td><input type="text" name="user_temp" pattern="[0-9]{1,}" required>
+                <select name="init_type">
+                    <option disabled>Select Temperature Type</option>
+                    <option value="farenheit">Farenheit</option>
+                    <option value="celsius">Celsius</option>
+                    <option value="kelvin">Kelvin</option>
+                </select>
+            </td>
 
- <label for="newScale">Desired scale?</label>
- <select name="newScale">
-<option value="" NULL
-<?php if(isset($_POST['newScale']) && $_POST['newScale'] == NULL) echo 'selected = "unselected" '
-;?>>
-<option value="fahr"<?php if(isset($_POST['newScale']) && $_POST['newScale'] == 'fahr') echo 'selected = "selected" '
-;?>>Fahrenheit</option>
-<option value="cel"<?php if(isset($_POST['newScale']) && $_POST['newScale'] == 'cel') echo 'selected = "selected" '
-;?>>Celcius</option> 
-<option value="kel"<?php if(isset($_POST['newScale']) && $_POST['newScale'] == 'kel') echo 'selected = "selected" '
-;?>>Kelvin</option> 
-<option value="ran"<?php if(isset($_POST['newScale']) && $_POST['newScale'] == 'ran') echo 'selected = "selected" '
-;?>>Rankine</option> 
-</select> 
-<span class="error"><?php echo $newScale_Err; ?></span>
-
- <input type = "submit" value = "Convert"/> 
- </form> 
+        </tr>
+        <tr>
+            <td>See the temperature in:</td>
+            <td><select name="temp_type" id="temp_type">
+                    <option disabled>Select Temperature Type</option>
+                    <option value="farenheit">Farenheit</option>
+                    <option value="celsius">Celsius</option>
+                    <option value="kelvin">Kelvin</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td><input type="submit" name="convert" id="convert" value="Convert"></td>
+            <td><input type="reset" name="reset" id="reset" value="Reset"></td>
+        </tr>
+    </table>
+</form>
