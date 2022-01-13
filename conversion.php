ed5dab9686445c96e8813611a4c01a182d50b1cb
brinkbrink <brinkbrink@gmail.com>
2022-01-13 14:14:34 -0800

    error messages, etc

--- conversion.php
+++ conversion.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,28 +9,35 @@
 
 <body>
 <?php
-  include('includes/form.php');
+  include('includes/form.php'); // include form
 
   class Form
   {
     public function __construct($title, $method, $action, $list) {
+      // assigns properties from args passed during instantiation
       $this->title = $title;
       $this->method = $method;
       $this->action = $action;
       $this->list = $list;
     }
   }
+  // declares ConverterForm class
   class ConverterForm
   {
+    // constructor takes in object of type Form
     public function __construct(Form $form) {
+      // assigns title property from form
       $this->title = $form->title;
+      // define standard units of conversion for this form's calculations
       $this->unit = 1.8;
       $this->f_base = 32;
       $this->k_base = 273.15;
       $this->r_base = 459.67;
+      // define strings for form output and errors
       $this->out_Str = '';
       $this->error_msg = '';
     }
+    // conversion function for fahrenheit (we already know that the value-to-convert is fahrenheit because generateOutput() sessed out the first scale value and called this function accordingly)
     private function fahrenheitTo($input, $scale)
     {
       if ($scale == "cel") {
@@ -44,6 +50,7 @@
         return number_format(((int)$input), 2); 
       }
     }
+    // conversion function for celsius
     private function celsiusTo($input, $scale)
     {
       if ($scale == "fahr") {
@@ -56,6 +63,7 @@
         return number_format(((int)$input), 2);
       }
     }
+    // conversion function for kelvin
     private function kelvinTo($input, $scale)
     {
       if ($scale == "fahr") {
@@ -68,6 +76,7 @@
         return number_format(((int)$input), 2);
       }
     }
+    // conversion function for rankine
     private function rankineTo($input, $scale)
     {
       if ($scale == "fahr") {
@@ -80,6 +89,7 @@
         return number_format(((int)$input), 2);
       }
     }
+    // function to decide which form method to call based on string values of scale arguments
     public function generateOutput($scale1, $scale2, $conv) {
       if ($scale1 == "fahr") {
         $converted_out = $this->fahrenheitTo($conv, $scale2);
@@ -93,57 +103,47 @@
       if ($scale1 == "ran") {
         $converted_out = $this->rankineTo($conv, $scale2);
       }
+      // return converted value
       echo '<div class="form-tag"><p>The answer is '. $converted_out .' degrees.</p></div>';
     }
-    public function generateHTML() {
-      echo '
-      <fieldset>
-        <h1>'.$this->title.'</h1>
-        <div class="form-row">
-          <div class="form-group">
-            <input type="number" name="degree" size=4 placeholder="degrees">
-            <select class="select">
-  
-            </select>
-          </div>
-          <div class="form-group">
-            <input type="number" name="degree" size=4 placeholder="degrees">
-            <select class="select"></select>
-          </div>
-        </div>
-      </fieldset>
-      ';
-    }
-  }
+  } // end ConverterForm class
+  // declares an array to store form data, to be passed to the Form constructor to generate form fields (we didn't get as far along with implementing this concept, but this is the key to creating multiple forms with a variable number of form fields)
   $form_data = array(
     'degree' => '',
-    'ogScale' => '',
-    'newScale' => ''
+    'original' => '',
+    'desired' => ''
   );
-  
+  // instantiate new Form
   $form = new Form("Temperature Converter", "POST", "", $form_data);
   $temperature_form = new ConverterForm($form);
   
+  // declare arbitrary strings for readability
   $degree = '';
-  $ogScale = '';
-  $newScale = '';
+  $original = '';
+  $desired = '';
   
+  // Check if request is for POST ops
   if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // iterate form list
     foreach($form_data as $key => $val) {
+      // check if the form field is empty
       if (empty( $_POST[$key] )) {
+        // if it is: throw an error indicating the missing field
         echo "<span class='error'>$key is required!</span>\n";
       } else {
+        // if it isn't, assign the value of the input form field to $degree
         $degree = $_POST[$key];
       }
     }
-  
+  // check if values in POST array is set
     if(isset($_POST['degree'])) {
       $degree = $_POST['degree'];
-      $ogScale = $_POST['ogScale'];
-      $newScale = $_POST['newScale'];
+      $original = $_POST['original'];
+      $desired = $_POST['desired'];
   
       if(($degree != NULL) && (is_numeric($degree))){ // making sure degree value is filled and numeric
-        $temperature_form->generateOutput($ogScale, $newScale, $degree);
+        $temperature_form->generateOutput($original
+        , $desired, $degree); // call on converter handler method to perform the appropriate operation for the form data
       } // end inner if
       elseif((is_numeric($degree)) == FALSE) {
         echo '<span class="error">Please enter a numeric value!</span>';
--- includes/form.php
+++ includes/form.php
@@ -5,39 +5,36 @@
 <fieldset>
     <legend>A Temperature Converter</legend>
     <label for="degree">How many degrees?</label>
-    <input type = "text" name = "degree" size=4> 
-    <span class="error"><?php echo $degree_Err; ?></span>
+    <input type = "text" name = "degree" size=4>
 
- <label for="ogScale">Original scale?</label>
- <select name="ogScale"><option value="" NULL
-<?php if(isset($_POST['ogScale']) && $_POST['ogScale'] == NULL) echo 'selected = "unselected" '
+ <label for="original">Original scale?</label>
+ <select name="original"><option value="" NULL
+<?php if(isset($_POST['original']) && $_POST['original'] == NULL) echo 'selected = "unselected" '
 ;?>>
-<option value="fahr"<?php if(isset($_POST['ogScale']) && $_POST['ogScale'] == 'fahr') echo 'selected = "selected" '
+<option value="fahr"<?php if(isset($_POST['original']) && $_POST['original'] == 'fahr') echo 'selected = "selected" '
 ;?>>Fahrenheit</option>
-<option value="cel"<?php if(isset($_POST['ogScale']) && $_POST['ogScale'] == 'cel') echo 'selected = "selected" '
+<option value="cel"<?php if(isset($_POST['original']) && $_POST['original'] == 'cel') echo 'selected = "selected" '
 ;?>>Celcius</option> 
-<option value="kel"<?php if(isset($_POST['ogScale']) && $_POST['ogScale'] == 'kel') echo 'selected = "selected" '
+<option value="kel"<?php if(isset($_POST['original']) && $_POST['original'] == 'kel') echo 'selected = "selected" '
 ;?>>Kelvin</option> 
-<option value="ran"<?php if(isset($_POST['ogScale']) && $_POST['ogScale'] == 'ran') echo 'selected = "selected" '
+<option value="ran"<?php if(isset($_POST['original']) && $_POST['original'] == 'ran') echo 'selected = "selected" '
 ;?>>Rankine</option> 
 </select> 
-<span class="error"><?php echo $ogScale_Err; ?></span>
 
- <label for="newScale">Desired scale?</label>
- <select name="newScale">
+ <label for="desired">Desired scale?</label>
+ <select name="desired">
 <option value="" NULL
-<?php if(isset($_POST['newScale']) && $_POST['newScale'] == NULL) echo 'selected = "unselected" '
+<?php if(isset($_POST['desired']) && $_POST['desired'] == NULL) echo 'selected = "unselected" '
 ;?>>
-<option value="fahr"<?php if(isset($_POST['newScale']) && $_POST['newScale'] == 'fahr') echo 'selected = "selected" '
+<option value="fahr"<?php if(isset($_POST['desired']) && $_POST['desired'] == 'fahr') echo 'selected = "selected" '
 ;?>>Fahrenheit</option>
-<option value="cel"<?php if(isset($_POST['newScale']) && $_POST['newScale'] == 'cel') echo 'selected = "selected" '
+<option value="cel"<?php if(isset($_POST['desired']) && $_POST['desired'] == 'cel') echo 'selected = "selected" '
 ;?>>Celcius</option> 
-<option value="kel"<?php if(isset($_POST['newScale']) && $_POST['newScale'] == 'kel') echo 'selected = "selected" '
+<option value="kel"<?php if(isset($_POST['desired']) && $_POST['desired'] == 'kel') echo 'selected = "selected" '
 ;?>>Kelvin</option> 
-<option value="ran"<?php if(isset($_POST['newScale']) && $_POST['newScale'] == 'ran') echo 'selected = "selected" '
+<option value="ran"<?php if(isset($_POST['desired']) && $_POST['desired'] == 'ran') echo 'selected = "selected" '
 ;?>>Rankine</option> 
 </select> 
-<span class="error"><?php echo $newScale_Err; ?></span>
 
  <input type = "submit" value = "Convert"/> 
  </form> 
